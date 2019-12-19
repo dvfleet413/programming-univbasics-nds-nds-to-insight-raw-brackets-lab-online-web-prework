7ef174c914a3a4d8e409c80a77a9340cd639e4ab
David Van Fleet <dvfleet413@gmail.com>
2019-12-19 02:08:55 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -7,7 +7,7 @@ def directors_totals(nds)
   #
   #
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
-  result = {
+  result = {pp nds}
   }
   #
   # Use loops, variables and the accessing method, [], to loop through the NDS
@@ -18,5 +18,5 @@ def directors_totals(nds)
   #
   #
   # Be sure to return the result at the end!
-  nil
+  result
 end
